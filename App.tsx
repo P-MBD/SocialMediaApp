3c4221544908f7749cb958cf9593b4cb1c5f20be
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-08-22 10:35:09 +0330

    73 - Implementing Infinite Scroll73 - Implementing Infinite Scroll

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import React,{useState} from 'react';
-import {SafeAreaView, Text, ScrollView,View, Pressable, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+
+//Custom Components
 import Title from './components/Title/Title';
 import UserStory from './components/UserStory/UserStory';
+
+//FontAwesome Icons
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
-import style from './assets/styles/main'
+
+//Style Imports
+import style from './assets/styles/main';
+
 const App = () => {
-  //All of items in our stories
+  //All of the items in our stories
   const data = [
     {
       firstName: 'Joseph',
@@ -45,28 +59,90 @@ const App = () => {
       id: 9,
     },
   ];
-const pageSize=4;
-const [pageNumber, setPageNumber] = useState(1);
-const [isLoading, setIsLoading] = useState(false);
-const [renderData,setRenderData]= useState([]);
+  // Define page size constant for the number of items to be displayed per page
+  const pageSize = 4;
+
+  // Define state variable for the current page number
+  // so that we know how many pages we have fetched already
+  const [pageNumber, setPageNumber] = useState(1);
+
+  // Define state variable for the loading status of the flatlist,
+  // will be used when we'll be fetching data on scroll until we complete the fetch
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Define state variable for the data to be rendered on the page
+  const [renderedData, setRenderedData] = useState(data.slice(0, pageSize));
+
+  /**
+   * function that returns the data for the page to be fetched
+   * @param data - all the data
+   * @param pageNumber - page number to fetch
+   * @param pageSize - number of items to fetch for the page
+   */
+  const pagination = (data, pageNumber, pageSize) => {
+    let startIndex = (pageNumber - 1) * pageSize;
+    console.log(startIndex, renderedData.length)
+    //don't return the information that does not exist inside the data array
+    if (startIndex >= data.length) {
+      return [];
+    }
+    //set the page number, to the page number that we wanted to fetch so that we have information
+    //about which page was the one that was last fetched
+    setPageNumber(pageNumber);
+    return data.slice(startIndex, startIndex + pageSize);
+  };
+
   return (
+    // Use the SafeAreaView component to ensure content is displayed within the safe area boundaries of the device
     <SafeAreaView>
-        <ScrollView>
-           <View style={style.header}>
-             <Title title={"Lets Explore"} />
-             <Pressable style={style.messageIcon}>
-                 <FontAwesomeIcon icon={faEnvelope} color='#CACDDE' size={20} />
-                 <View style={style.messageNumberContainer}>
-                     <Text style={{fontSize:6,fontFamily: 'Inter',fontWeight: '600',lineHeight:7,color:'#FFFFFF'}} >2</Text>
-                 </View>
-                 
-             </Pressable>
-             
-           </View>
-           <View style={style.userStoryContainer}>
-              <FlatList   showsHorizontalScrollIndicator={false} horizontal={true} data={data}  renderItem={({item}) => <UserStory firstName={item.firstName} />}/>
-           </View>
-        </ScrollView>
+      {/* Use ScrollView to allow users to scroll through content */}
+      <ScrollView>
+        {/* Use View to create a container for title and icon */}
+        <View style={style.header}>
+          {/* Use custom Title component to display the title */}
+          <Title title={"Let's Explore"} />
+          {/* Use Pressable to create a clickable component */}
+          <Pressable style={style.messageIcon}>
+            {/* Use FontAwesomeIcon component to display an icon from FontAwesome icon set */}
+            <FontAwesomeIcon icon={faEnvelope} color={'#CACDDE'} size={20} />
+            {/* Use View to create a container for message number */}
+            <View style={style.messageNumberContainer}>
+              {/* Use Text to display the number of messages */}
+              <Text style={style.messageNumber}>2</Text>
+            </View>
+          </Pressable>
+        </View>
+        {/* Use View to create a container for the user stories */}
+        <View style={style.userStoryContainer}>
+          {/* Use FlatList to display user stories */}
+          <FlatList
+            //when the user scrolls through half of the data call onEndReached function
+            onEndReachedThreshold={0.5}
+            keyExtractor={item => item.id.toString()}
+            onEndReached={() => {
+              //if we are not already in the middle of fetching data then fetch the data
+              if (!isLoading) {
+                //set is loading to true because we just started fetching data
+                setIsLoading(true);
+                setRenderedData(prev => [
+                  ...prev,
+                  ...pagination(data, pageNumber + 1, pageSize),
+                ]);
+                //after updating rendered data we have to set is loading to false, because we loaded the data we needed
+                setIsLoading(false);
+              }
+            }}
+            // Hide horizontal scroll indicator
+            showsHorizontalScrollIndicator={false}
+            // Set FlatList to display horizontally
+            horizontal={true}
+            // Pass in data to be rendered in FlatList
+            data={renderedData}
+            // Define how each item should be rendered
+            renderItem={({item}) => <UserStory firstName={item.firstName} />}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
