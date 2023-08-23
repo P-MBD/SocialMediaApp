361609387130602ef0202b6f8e4ec0bb36d1ae4e
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-08-23 18:50:28 +0330

    74 - Setting Up for User Posts English

--- assets/styles/main.js
+++ assets/styles/main.js
@@ -1,36 +1,56 @@
-import { StyleSheet } from "react-native";
+// Import StyleSheet from react-native
+import {StyleSheet} from 'react-native';
+
+// Define a new StyleSheet object
 const style = StyleSheet.create({
-    header:{
-            paddingTop:30,
-            paddingRight:26,
-            paddingLeft:17,
-            flex:1, 
-            flexDirection:'row',
-            alignItems:'center',
-            justifyContent:'space-between'     
-    },
-    messageIcon:{
-        backgroundColor:'#F9FAFB',
-        padding:12,
-        borderRadius:100,
-    },
-    messageNumberContainer:{
-        width:10,
-        height:10,
-        backgroundColor:'#F35BAC',
-        borderRadius:10, 
-        flex:1,
-       flexDirection:'row',
-       alignItems:'center',
-       justifyContent:'center',
-       position:'absolute',
-       right:7,
-       top:10,
-       },
-       userStoryContainer:{
-        paddingHorizontal:28,
-        marginTop:12,
-        
-       }
-})
+  // Define styles for the header container
+  header: {
+    paddingTop: 30,
+    paddingRight: 26,
+    paddingLeft: 17,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  // Define styles for the message icon
+  messageIcon: {
+    backgroundColor: '#F9FAFB',
+    padding: 12,
+    borderRadius: 100,
+  },
+  // Define styles for the message number container
+  messageNumberContainer: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#F35BAC',
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    right: 7,
+    top: 10,
+  },
+  // Define styles for the message number text
+  messageNumber: {
+    fontSize: 6,
+    fontFamily: 'Inter',
+    lineHeight: 7,
+    fontWeight: '600',
+    color: '#FFFFFF',
+  },
+  userStoryContainer: {
+    paddingHorizontal: 28,
+    marginTop: 12,
+    height: 100,
+  },
+  userPostContainer: {
+    marginTop: 30,
+    height: '100%',
+    paddingHorizontal: 24,
+  },
+});
+
 export default style;
